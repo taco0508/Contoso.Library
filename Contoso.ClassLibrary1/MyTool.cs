9fb53e155666e08e4b7d2a12a69f555559111b2f
Taco0508 <taco0508@gmail.com>
2018-07-09 00:35:57 +0800

    加入專案檔案。

---
+++ Contoso.ClassLibrary1/MyTool.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contoso.ClassLibrary1
+{
+    public class MyTool
+    {
+        public void Run()
+        {
+            Console.WriteLine("Hello World! 1.0.4");
+        }
+    }
+}
